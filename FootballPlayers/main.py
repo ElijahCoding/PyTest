394cfe5d8e080c3a6a4abfbb01fa5b0b4de823f7
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-14 19:55:37 +0300

    formatting data

--- FootballPlayers/main.py
+++ FootballPlayers/main.py
@@ -6,4 +6,6 @@ base_url = 'https://en.wikipedia.org/wiki/World_Soccer_(magazine)'
 
 page = requests.get(base_url)
 
-print(type(page))
+if page.status_code == requests.codes.ok:
+    bs = BeautifulSoup(page.text, 'lxml')
+    print(bs.prettify())
