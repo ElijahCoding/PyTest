ccaa7cf7a76f6ceabe738d638de57060d6ca1098
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-09 19:23:48 +0300

    @property and setter

--- OOP/Encapsulation.py
+++ OOP/Encapsulation.py
@@ -2,4 +2,16 @@ class MyObject:
     def __init__(self):
         self.__attribute = 0
 
-    
+    @property
+    def attribute(self):
+        return self.__attribute
+
+    @attribute.setter
+    def attribute(self, value):
+        if value < 100:
+            self.__attribute = value
+
+obj = MyObject()
+print(obj.attribute)
+obj.attribute = 10
+print(obj.attribute)
