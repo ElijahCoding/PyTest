15c356e783a970d884d02b61e99e203857bf39d4
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-10 00:30:24 +0300

    __getattribute__

--- OOP/Attribute.py
+++ OOP/Attribute.py
@@ -1,3 +1,15 @@
 class MyObject:
     def __init__(self):
         self.password = None
+
+    def __getattribute__(self, item):
+        if item == 'secret_field' and self.password == '123abc':
+            return 'Secret value'
+        else:
+            return object.__getattribute__(self, item)
+
+obj = MyObject()
+print(obj.password)
+obj.password = '123abc'
+print(obj.password)
+print(obj.secret_field)
