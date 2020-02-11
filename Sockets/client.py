3df242fb78019185add6ccde555d0690126d066a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-11 22:19:33 +0300

    buffering data, close clientsocket after using

--- Sockets/client.py
+++ Sockets/client.py
@@ -4,6 +4,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((socket.gethostname(), 3334))
 
-msg = s.recv(1024)
+full_msg = ''
+while True:
+    msg = s.recv(8)
+    if len(msg) <= 0:
+        break
+    full_msg += msg.decode('utf-8')
 
-print(msg.decode('utf-8'))
+print(full_msg)
