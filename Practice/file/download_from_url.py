c7d94fb0d1bcc5193635d3c94e3e2349131ec3e6
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-12 13:56:14 +0300

    download file

--- Practice/file/download_from_url.py
+++ Practice/file/download_from_url.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from download_util import download_file
 
 THIS_FILE_PATH = os.path.abspath(__file__)
 BASE_DIR = os.path.dirname(THIS_FILE_PATH)
@@ -9,7 +10,10 @@ os.makedirs(DOWNLOADS_DIR, exist_ok=True)
 downloaded_img_path = os.path.join(DOWNLOADS_DIR, 'test.jpg')
 url = "https://q-cf.bstatic.com/images/hotel/max1024x768/211/211636126.jpg"
 
+# a smallish item
 r = requests.get(url, stream=True)
-r.raise_for_status()
+r.raise_for_status() # 200
 with open(downloaded_img_path, 'wb') as f:
     f.write(r.content)
+
+download_file(url, DOWNLOADS_DIR)
--- Practice/file/download_util.py
+++ Practice/file/download_util.py
@@ -1,15 +1,24 @@
 import os
 import requests
+import shutil
 
-THIS_FILE_PATH = os.path.abspath(__file__)
-BASE_DIR = os.path.dirname(THIS_FILE_PATH)
-DOWNLOADS_DIR = os.path.join(BASE_DIR, 'downloads')
-os.makedirs(DOWNLOADS_DIR, exist_ok=True)
+def download_file(url, directory, fname=None):
+    if fname == None:
+        fname = os.path.basename(url)
+        dl_path = os.path.join(directory, fname)
+        with requests.get(url, stream=True) as r:
+            with open(dl_path, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+        return dl_path
 
-downloaded_img_path = os.path.join(DOWNLOADS_DIR, 'test.jpg')
-url = "https://q-cf.bstatic.com/images/hotel/max1024x768/211/211636126.jpg"
-
-r = requests.get(url, stream=True)
-r.raise_for_status()
-with open(downloaded_img_path, 'wb') as f:
-    f.writ(r.content)
+def download_file_slower(url):
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+                    # f.flush()
+    return local_filename
