4ad8804959a4ece4ff0d413064908646a9ffa245
ElijahCoding <cuiyeqing960904@gmail.com>
2020-06-23 22:13:22 +0300

    introduction to real threads

---
+++ Multithreading/basic_threads/threaded_prod.py
@@ -0,0 +1,21 @@
+import datetime
+import colorama
+import random
+import time
+import threading
+
+def generate_data(num: int, data: list):
+    for idx in range(1, num + 1):
+        item = idx * idx
+        data.append((item, datetime.datetime.now()))
+
+        print(colorama.Fore.YELLOW + f" -- generated item {idx}", flush=True)
+        time.sleep(random.random() + .5)
+
+def process_data(num: int, data: list):
+    processed = 0
+    while processed < num:
+        item = data.pop(0)
+        if not item:
+            time.sleep(.01)
+            contine
