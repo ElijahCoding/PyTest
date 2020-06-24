78013758d1a5a10310ae5ea52501efa630f37838
ElijahCoding <cuiyeqing960904@gmail.com>
2020-06-24 14:23:25 +0300

    thread

--- Multithreading/basic_threads/threaded_prod.py
+++ Multithreading/basic_threads/threaded_prod.py
@@ -4,6 +4,26 @@ import random
 import time
 import threading
 
+def main():
+    t0 = datetime.datetime.now()
+    print(colorama.Fore.WHITE + "App started.", flush=True)
+
+    data = []
+
+    threads = [
+        threading.Thread(target=generate_data, args=(20, data), daemon=True),
+        threading.Thread(target=generate_data, args=(20, data), daemon=True),
+        threading.Thread(target=process_data, args=(40, data), daemon=True),
+    ]
+
+    [t.start() for t in threads]
+    print("Started....")
+    [t.join() for t in threads]
+
+
+    dt = datetime.datetime.now() - t0
+    print(colorama.Fore.WHITE + "App exiting, total time: {:,.2f} sec.".format(dt.total_seconds()), flush=True)
+
 def generate_data(num: int, data: list):
     for idx in range(1, num + 1):
         item = idx * idx
@@ -18,4 +38,15 @@ def process_data(num: int, data: list):
         item = data.pop(0)
         if not item:
             time.sleep(.01)
-            contine
+            continue
+        processed += 1
+        value = item[0]
+        t = item[1]
+        dt = datetime.datetime.now() - t
+
+        print(colorama.Fore.CYAN +
+              " +++ Processed value {} after {:,.2f} sec.".format(value, dt.total_seconds()), flush=True)
+        time.sleep(.5)
+
+if __name__ == '__main__':
+    main()
