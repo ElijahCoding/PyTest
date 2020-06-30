c86a54036fe07c1af40b7b8f76ad5f421b894cec
ElijahCoding <cuiyeqing960904@gmail.com>
2020-06-30 22:10:58 +0300

    return values

--- AsyncMethods/cpu_attempt/compute_multiprocessing.py
+++ AsyncMethods/cpu_attempt/compute_multiprocessing.py
@@ -11,24 +11,32 @@ def main():
 
     processor_count = multiprocessing.cpu_count()
     pool = multiprocessing.Pool()
+    tasks = []
     for n in range(1, processor_count + 1):
-        pool.apply_async(
+        task = pool.apply_async(
             do_math,
             (30_000_000 * (n - 1) / processor_count, 30_000_000 * n / processor_count)
         )
+        tasks.append(task)
     pool.close()
     pool.join()
 
     dt = datetime.datetime.now() - t0
     print("Done in {:,.2f} sec.".format(dt.total_seconds()))
+    print("Our results: ")
+    for t in tasks:
+        print(t.get())
 
 
 def do_math(start=0, num=10):
     pos = start
     k_sq = 1000 * 1000
+    ave = 0
     while pos < num:
         pos += 1
-        dist = math.sqrt((pos - k_sq)*(pos - k_sq))
+        val = math.sqrt((pos - k_sq)*(pos - k_sq))
+        ave += val / num
+    return int(ave)
 
 
 if __name__ == '__main__':
