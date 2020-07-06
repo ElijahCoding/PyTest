12ba0c41f141f8eee1ef98132d86ced9f1d54bcf
ElijahCoding <cuiyeqing960904@gmail.com>
2020-07-06 14:33:17 +0300

    configs built

--- AsyncMethods/async_web/cityscape_api/app.py
+++ AsyncMethods/async_web/cityscape_api/app.py
@@ -1,10 +1,19 @@
 import flask
+from config import settings
 
 app = flask.Flask(__name__)
 is_debug = True
 
+def configure_app():
+    mode = 'dev' if is_debug else 'prod'
+    data = settings.load(mode)
+
+    print("Using cached data? {}".format(data.get('use_cached_data')))
+
 def run_web_app():
     app.run(debug=is_debug, port=5001)
+
+configure_app()
 
 if __name__ == '__main__':
     run_web_app()
