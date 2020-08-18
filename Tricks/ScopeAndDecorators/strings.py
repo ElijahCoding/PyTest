f39fe33b93ed32d3de07be735da135f2ef14df7a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-08-19 00:10:25 +0300

    strings zipping trick

---
+++ Tricks/ScopeAndDecorators/strings.py
@@ -0,0 +1,11 @@
+from string import ascii_lowercase
+
+# key = []
+# for i in range(len(ascii_lowercase)):
+#     key[ascii_lowercase[i]] = i+1
+
+# letters = ascii_lowercase
+#
+# num = list(range(1, 27))
+#
+# key = dict(zip(letters, num))
