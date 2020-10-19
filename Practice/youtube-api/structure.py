c224c8f20e8a0bbb83adb2f029ae5b0526e52526
ElijahCoding <cuiyeqing960904@gmail.com>
2020-10-19 23:58:51 +0300

    struucture built

---
+++ Practice/youtube-api/structure.py
@@ -0,0 +1,14 @@
+from googleapiclient.discovery import build
+
+api_key = ''
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+request = youtube.channels().list(
+    part='statistics',
+    forUsername='sentdex'
+)
+
+response = request.execute()
+
+print(response)
